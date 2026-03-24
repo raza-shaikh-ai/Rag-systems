d61688f6d8c238a6121bf82d32aaa8a2ceba43a2
Raza Shaikh <razashaikh@Razas-MacBook-Air.local>
2026-03-24 22:03:11 +0530

    new model

--- simpleai.py
+++ simpleai.py
@@ -4,8 +4,6 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 import os
-from onnxoptmized import ONNXChatModel
-
 load_dotenv()
 
 simple_prompt = PromptTemplate(
@@ -24,15 +22,7 @@ Answer:
     input_variables=["question"]
 )
 
-model = ONNXChatModel()
-runnable_model = RunnableLambda(model.invoke)
 
-parser = StrOutputParser()
-
-
-simple_chain = simple_prompt | runnable_model | parser
-
-"""
 model = ChatOpenAI(
     model="deepseek/deepseek-chat",
     api_key=os.getenv("openrouter").strip(),
@@ -40,5 +30,17 @@ model = ChatOpenAI(
     max_tokens=500,
     temperature=0.4
 )
-"""
+
+parser = StrOutputParser()
+
+
+simple_chain = simple_prompt | model | parser
+
+model = ChatOpenAI(
+    model="deepseek/deepseek-chat",
+    api_key=os.getenv("openrouter").strip(),
+    base_url="https://openrouter.ai/api/v1",
+    max_tokens=500,
+    temperature=0.4
+)
 
